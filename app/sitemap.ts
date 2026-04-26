56b85061e105fdaf6379a0769e7b6723c75281cb
Sanchit Gupta <sanchitgupta2512@gmail.com>
2026-04-26 19:19:52 +0530

    changed to previous dynamic sitemap and updated the cache issue

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,3 +1,6 @@
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 import { MetadataRoute } from 'next'
 import { icons } from '../lib/icons'
 import { getAllPosts } from '../lib/blog'
