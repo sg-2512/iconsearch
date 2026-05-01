6db7f98e0d22af02ddb7d124eba7c90477b94627
Sanchit Gupta <sanchitgupta2512@gmail.com>
2026-05-01 11:54:40 +0530

    sitemap correction

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,3 @@
-export const dynamic = 'force-dynamic'
-export const revalidate = 0
-
 import { MetadataRoute } from 'next'
 import { icons } from '../lib/icons'
 import { getAllPosts } from '../lib/blog'
